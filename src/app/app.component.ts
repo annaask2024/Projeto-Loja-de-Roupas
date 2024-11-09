c4ea9fc67a454829efe04af3e3e846e6864f63f5
Alana Alexia Santana Tavares <03348970@sempreuninassau.com.br>
2024-11-08 21:32:57 -0300

    redefinir senha

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RedefinirSenhaComponent } from './components/redefinir-senha/redefinir-senha.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RedefinirSenhaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
