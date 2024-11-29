635be879fe2da22b4d52fece24b83d21343c234d
annaask2024 <aninhaxuxu@outlook.com.br>
2024-11-29 20:50:27 -0300

    Trabalho Front

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { PaginaPrincipalComponent } from './pagina-principal/pagina-principal.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '', component: LoginComponent
+    },
+    {
+        path: 'principal', component: PaginaPrincipalComponent
+    }
+];
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,5 +9,12 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  constructor(private router: Router){
+
+  }
+
+  navegar(){
+    this.router.navigate(['principal'])
+  }
 
 }
